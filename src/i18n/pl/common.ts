d1ec88f7ab5e6a38ce7e792e622e1e23427e80b6
Adam Czerwonka <adamczerwonka59@gmail.com>
2024-11-13 22:54:37 +0100

    fest: i18n configuration

---
+++ src/i18n/pl/common.ts
@@ -0,0 +1,18 @@
+const menu = {
+  courses: "Przedmioty",
+  networks: "Sieci",
+  reservations: "Rezerwacje",
+  teams: "Zespoły",
+  resourceGroups: "Grupy zasobów",
+  resourceGroupPools: "Pule grup zasobów",
+  home: "Strona główna",
+};
+
+const resourceGroupPools = {
+  title: "Pule grup zasobów",
+};
+
+export default {
+  menu,
+  resourceGroupPools,
+};
