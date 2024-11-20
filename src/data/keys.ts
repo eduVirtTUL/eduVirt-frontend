ef3f5692ca6a1a77a4d8dd79c664fb0bb9f9fba3
Michał Piestrzeniewicz <michal.p2608@wp.pl>
2024-11-20 23:01:57 +0100

    Add: Add extended handling vlan ranges

--- src/data/keys.ts
+++ src/data/keys.ts
@@ -2,4 +2,5 @@ export const keys = {
   COURSE: "course",
   RESOURCE_GROUP: "resourceGroup",
   VNIC_PROFILE: "vnicProfile",
+  VLANS_RANGE: "vlansRange",
 };
--- src/pages/VlanRanges/index.tsx
+++ src/pages/VlanRanges/index.tsx
@@ -1,16 +1,49 @@
 import PageHeader from "@/components/PageHeader.tsx";
+import {useDialog} from "@/stores/dialogStore.ts";
+import {Button} from "@/components/ui/button.tsx";
+import {PlusIcon} from "lucide-react";
+import CreateVlansRangeModal from "@/components/Modals/CreateVlansRangeModal.tsx";
+import {useVlansRanges} from "@/data/network/useVlansRanges.ts";
+import {Card, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
 
 const VlanRangesPage: React.FC = () => {
-    // const {vnicProfiles, isLoading} = useVnicProfiles();
+    const {vlansRanges, isLoading} = useVlansRanges();
+    const { open } = useDialog();
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
-    // if (isLoading) {
-    //     return <div>Loading...</div>;
-    // }
-
-    /// TODO pagination
     return (
         <>
+            <CreateVlansRangeModal/>
             <PageHeader title="VLAN ranges"/>
+            <div className="pb-5">
+                <Button
+                    onClick={() => {
+                        open("createVlansRange");
+                    }}
+                >
+                    <PlusIcon/>
+                    New VLANs range
+                </Button>
+            </div>
+
+            <div className="flex flex-wrap gap-5 w-full">
+                {(vlansRanges?.length == 0 ? [] : vlansRanges)?.map((vlansRange) => (
+                    <Card key={vlansRange.id} className="w-80">
+                        <CardHeader>
+                            <CardTitle>Range</CardTitle>
+                            <CardDescription>{vlansRange.from}-{vlansRange.to}</CardDescription>
+                        </CardHeader>
+                        <CardFooter>
+                            <Button
+                                onClick={() => {alert("Removed!")}}>
+                                Remove
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                ))}
+            </div>
         </>
     );
 };
