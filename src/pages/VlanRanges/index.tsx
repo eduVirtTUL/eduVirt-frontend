c9b65aca6270c23438b6ef4d3278b9f7c56e59b8
Adam Czerwonka <adamczerwonka59@gmail.com>
2024-12-02 23:40:48 +0100

    chore: after merge cleanup

--- src/pages/VlanRanges/index.tsx
+++ src/pages/VlanRanges/index.tsx
@@ -1,72 +1,76 @@
 import PageHeader from "@/components/PageHeader.tsx";
-import {useDialog} from "@/stores/dialogStore.ts";
-import {Button} from "@/components/ui/button.tsx";
-import {PlusIcon, Undo2} from "lucide-react";
+import { useDialog } from "@/stores/dialogStore.ts";
+import { Button } from "@/components/ui/button.tsx";
+import { PlusIcon, Undo2 } from "lucide-react";
 import CreateVlansRangeModal from "@/components/Modals/CreateVlansRangeModal.tsx";
-import {useVlansRanges} from "@/data/network/useVlansRanges.ts";
-import {Card, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
-import {useRemoveVlansRange} from "@/data/network/useRemoveVlansRange.ts";
-import {Link} from "react-router-dom";
+import { useVlansRanges } from "@/data/network/useVlansRanges.ts";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card.tsx";
+import { useRemoveVlansRange } from "@/data/network/useRemoveVlansRange.ts";
+import { Link } from "react-router";
 import React from "react";
 
 const VlanRangesPage: React.FC = () => {
-    const {vlansRanges, isLoading} = useVlansRanges();
-    const {open} = useDialog();
-    const {removeVlansRangeAsync} = useRemoveVlansRange();
+  const { vlansRanges, isLoading } = useVlansRanges();
+  const { open } = useDialog();
+  const { removeVlansRangeAsync } = useRemoveVlansRange();
 
-    const handleRemoveVlansRange = (async (vlansRangeId?: string) => {
-        if (vlansRangeId) {
-            await removeVlansRangeAsync(vlansRangeId);
-        }
-    });
-
-    if (isLoading) {
-        return <div>Loading...</div>;
+  const handleRemoveVlansRange = async (vlansRangeId?: string) => {
+    if (vlansRangeId) {
+      await removeVlansRangeAsync(vlansRangeId);
     }
+  };
 
-    return (
-        <>
-            <CreateVlansRangeModal/>
-            <div>
-                <Button asChild variant="outline" size="icon">
-                    <Link to={"/networks"}>
-                        <Undo2/>
-                    </Link>
-                </Button>
-            </div>
-            <PageHeader title="VLAN ranges"/>
-            <div className="pb-5">
-                <Button
-                    onClick={() => {
-                        open("createVlansRange");
-                    }}
-                >
-                    <PlusIcon/>
-                    New VLANs range
-                </Button>
-            </div>
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-            <div className="flex flex-wrap gap-5 w-full">
-                {
-                    (vlansRanges?.length ? vlansRanges : [])?.map((vlansRange) => (
-                        <Card key={vlansRange.id} className="w-80">
-                            <CardHeader>
-                                <CardTitle>Range</CardTitle>
-                                <CardDescription>{vlansRange.from}-{vlansRange.to}</CardDescription>
-                            </CardHeader>
-                            <CardFooter>
-                                <Button
-                                    onClick={() => handleRemoveVlansRange(vlansRange.id)}
-                                >
-                                    Remove
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    ))
-                }
-            </div>
-        </>
-    );
+  return (
+    <>
+      <CreateVlansRangeModal />
+      <div>
+        <Button asChild variant="outline" size="icon">
+          <Link to={"/networks"}>
+            <Undo2 />
+          </Link>
+        </Button>
+      </div>
+      <PageHeader title="VLAN ranges" />
+      <div className="pb-5">
+        <Button
+          onClick={() => {
+            open("createVlansRange");
+          }}
+        >
+          <PlusIcon />
+          New VLANs range
+        </Button>
+      </div>
+
+      <div className="flex flex-wrap gap-5 w-full">
+        {(vlansRanges?.length ? vlansRanges : [])?.map((vlansRange) => (
+          <Card key={vlansRange.id} className="w-80">
+            <CardHeader>
+              <CardTitle>Range</CardTitle>
+              <CardDescription>
+                {vlansRange.from}-{vlansRange.to}
+              </CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Button onClick={() => handleRemoveVlansRange(vlansRange.id)}>
+                Remove
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default VlanRangesPage;
--- src/pages/VnicProfiles/index.tsx
+++ src/pages/VnicProfiles/index.tsx
@@ -1,124 +1,126 @@
 import PageHeader from "@/components/PageHeader.tsx";
-import {useVnicProfiles} from "@/data/network/useVnicProfiles.ts";
-import {Checkbox} from "@/components/ui/checkbox.tsx";
-import {Button} from "@/components/ui/button.tsx";
-import {useAddVnicProfileToPool} from "@/data/network/useAddVnicProfileToPool.ts";
-import {useRemoveVnicProfileFromPool} from "@/data/network/useRemoveVnicProfileFromPool.ts";
-import {Link} from "react-router-dom";
+import { useVnicProfiles } from "@/data/network/useVnicProfiles.ts";
+import { Checkbox } from "@/components/ui/checkbox.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { useAddVnicProfileToPool } from "@/data/network/useAddVnicProfileToPool.ts";
+import { useRemoveVnicProfileFromPool } from "@/data/network/useRemoveVnicProfileFromPool.ts";
+import { Link } from "react-router";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu.tsx";
-import {ArrowUpDown, MoreHorizontal} from "lucide-react";
-import DataTable from "@/pages/Courses/DataTable.tsx";
-import {ColumnDef} from "@tanstack/react-table";
-import {VnicProfileDto} from "@/api";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import DataTable from "@/components/DataTable";
+import { ColumnDef } from "@tanstack/react-table";
+import { VnicProfileDto } from "@/api";
 import React from "react";
 
 const VnicProfilesPage: React.FC = () => {
-    const {vnicProfiles, isLoading} = useVnicProfiles();
-    const {addVnicProfileToPoolAsync} = useAddVnicProfileToPool();
-    const {removeVnicProfileFromPoolAsync} = useRemoveVnicProfileFromPool();
+  const { vnicProfiles, isLoading } = useVnicProfiles();
+  const { addVnicProfileToPoolAsync } = useAddVnicProfileToPool();
+  const { removeVnicProfileFromPoolAsync } = useRemoveVnicProfileFromPool();
 
-    const handleAddVnicProfile = (async (vnicProfileId?: string) => {
-        if (vnicProfileId) {
-            await addVnicProfileToPoolAsync(vnicProfileId);
-        }
-    });
-
-    const handleRemoveVnicProfileFromPool = (async (vnicProfileId?: string) => {
-        if (vnicProfileId) {
-            await removeVnicProfileFromPoolAsync(vnicProfileId);
-        }
-    });
-
-    const columns: ColumnDef<VnicProfileDto>[] = [
-        {
-            accessorKey: "name",
-            header: ({column}) => {
-                return (
-                    <Button
-                        variant="ghost"
-                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-                    >
-                        Vnic profile name
-                        <ArrowUpDown className="ml-2 h-4 w-4"/>
-                    </Button>
-                );
-            },
-        },
-        //TODO add sorting for other columns / multi sort
-        {accessorKey: "networkName", header: "Network name"},
-        {accessorKey: "networkVlanId", header: "Network VLAN ID"},
-        //TODO add filtering for flag values
-        {
-            header: "In pool",
-            cell: ({row}) => {
-                const vnicProfile = row.original;
-
-                return (
-                    <Checkbox disabled={true} checked={vnicProfile.inPool}/>
-                );
-            },
-        },
-        {
-            id: "actions",
-            cell: ({row}) => {
-                const vnicProfile = row.original;
-
-                return (
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="h-8 w-8 p-0">
-                                <span className="sr-only">Open menu</span>
-                                <MoreHorizontal className="h-4 w-4"/>
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuItem>
-                                <Button variant="ghost"
-                                        disabled={vnicProfile.inPool}
-                                        onClick={() => handleAddVnicProfile(vnicProfile.id)}
-                                >
-                                    Add to pool
-                                </Button>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <Button variant="ghost"
-                                        disabled={!vnicProfile.inPool}
-                                        onClick={() => handleRemoveVnicProfileFromPool(vnicProfile.id)}
-                                >
-                                    Remove from pool
-                                </Button>
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                );
-            },
-        },
-    ];
-
-    if (isLoading) {
-        return <div>Loading...</div>;
+  const handleAddVnicProfile = async (vnicProfileId?: string) => {
+    if (vnicProfileId) {
+      await addVnicProfileToPoolAsync(vnicProfileId);
     }
+  };
 
-    /// TODO pagination
-    /// TODO add displaying extended info about selected vnic profile (isInUse, maybe id etc...)
-    return (
-        <>
-            <PageHeader title="Vnic profiles"/>
+  const handleRemoveVnicProfileFromPool = async (vnicProfileId?: string) => {
+    if (vnicProfileId) {
+      await removeVnicProfileFromPoolAsync(vnicProfileId);
+    }
+  };
 
-            <div className="pb-5">
-                <Button asChild>
-                    <Link to={'/networks/vlans'}>VLAN ranges</Link>
+  const columns: ColumnDef<VnicProfileDto>[] = [
+    {
+      accessorKey: "name",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Vnic profile name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+    },
+    //TODO add sorting for other columns / multi sort
+    { accessorKey: "networkName", header: "Network name" },
+    { accessorKey: "networkVlanId", header: "Network VLAN ID" },
+    //TODO add filtering for flag values
+    {
+      header: "In pool",
+      cell: ({ row }) => {
+        const vnicProfile = row.original;
+
+        return <Checkbox disabled={true} checked={vnicProfile.inPool} />;
+      },
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const vnicProfile = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>
+                <Button
+                  variant="ghost"
+                  disabled={vnicProfile.inPool}
+                  onClick={() => handleAddVnicProfile(vnicProfile.id)}
+                >
+                  Add to pool
                 </Button>
-            </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button
+                  variant="ghost"
+                  disabled={!vnicProfile.inPool}
+                  onClick={() =>
+                    handleRemoveVnicProfileFromPool(vnicProfile.id)
+                  }
+                >
+                  Remove from pool
+                </Button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
 
-            <DataTable data={vnicProfiles ?? []} columns={columns}/>
-        </>
-    );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  /// TODO pagination
+  /// TODO add displaying extended info about selected vnic profile (isInUse, maybe id etc...)
+  return (
+    <>
+      <PageHeader title="Vnic profiles" />
+
+      <div className="pb-5">
+        <Button asChild>
+          <Link to={"/networks/vlans"}>VLAN ranges</Link>
+        </Button>
+      </div>
+
+      <DataTable data={vnicProfiles ?? []} columns={columns} />
+    </>
+  );
 };
 
 export default VnicProfilesPage;
