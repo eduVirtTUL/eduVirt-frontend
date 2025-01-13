6d2a8d02e6cbc2f34ab661efb7445714cab76f2a
Adam Czerwonka <adamczerwonka59@gmail.com>
2025-01-13 12:55:28 +0100

    chore: remove unused code

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -112,26 +112,6 @@ const DataTable = <TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      {/*<div className="flex items-center justify-end space-x-2 py-4">*/}
-      {/*  <Button*/}
-      {/*    type="button"*/}
-      {/*    variant="outline"*/}
-      {/*    size="sm"*/}
-      {/*    onClick={() => table.previousPage()}*/}
-      {/*    disabled={!table.getCanPreviousPage()}*/}
-      {/*  >*/}
-      {/*    Previous*/}
-      {/*  </Button>*/}
-      {/*  <Button*/}
-      {/*    type="button"*/}
-      {/*    variant="outline"*/}
-      {/*    size="sm"*/}
-      {/*    onClick={() => table.nextPage()}*/}
-      {/*    disabled={!table.getCanNextPage()}*/}
-      {/*  >*/}
-      {/*    Next*/}
-      {/*  </Button>*/}
-      {/*</div>*/}
     </div>
   );
 };
