0559520e590d751e11d0c21460b61b30b382dcc7
jakkozi <jakub.kozielski02@gmail.com>
2025-01-18 14:02:43 +0100

    Env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_BASENAME: string,
+    readonly VITE_API_LOCATION: string,
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
